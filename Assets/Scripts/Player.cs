1f80feda3024d796a34dc2f1df7da5dc9da5872f
Anna Bertolani <bertolani.anna@gmail.com>
2019-06-24 14:56:05 +0200

    inizio main menu + fix roba

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-    public  int Life;
+    public int Life;
     public float MovementSpeed;
     public int Coins;
     public float JumpForce;
     public bool isGrounded;
     public Rigidbody2D rb;
     public Animator anim;
-
     bool isAlive = true;
+    public Vector3 StartEdge;
+    public Vector3 EndEdge;
 
     // Update is called once per frame
     void Update()
     {
-        if (Life > 0) {
+        if (Life > 0)
+        {
             CheckInput();
         }
     }
@@ -27,18 +30,24 @@ public class Player : MonoBehaviour {
     /// </summary>
     public void CheckInput()
     {
-        if (isAlive == false) {
+        if (isAlive == false)
+        {
             return;
         }
 
         // Movimento laterale
-        if (Input.GetKey(KeyCode.A) == true) { // SX
-            transform.position = transform.position + new Vector3(-MovementSpeed, 0f, 0f);                                   
+        if (Input.GetKey(KeyCode.A) == true && transform.position.x > StartEdge.x)
+        { // SX
+            transform.position = transform.position + new Vector3(-MovementSpeed, 0f, 0f);
             anim.SetInteger("movement/idle", -1);
-        } else if (Input.GetKey(KeyCode.D)) { // DX
+        }
+        else if (Input.GetKey(KeyCode.D) && transform.position.x < EndEdge.x)
+        { // DX
             transform.position = transform.position + new Vector3(+MovementSpeed, 0f, 0f);
             anim.SetInteger("movement/idle", 1);
-        } else {
+        }
+        else
+        {
             anim.SetInteger("movement/idle", 0);
         }
 
@@ -49,20 +58,24 @@ public class Player : MonoBehaviour {
             // transform.position = transform.position + new Vector3(0f, JumpForce * Time.deltaTime, 0f);
             isGrounded = false;
         }
-        if (Input.GetKey(KeyCode.S))
+        /*if (Input.GetKey(KeyCode.S))
         {
             Debug.Log("Accoviacciamento");
-        }
+        }*/
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-       
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         isGrounded = true;
-      
+        /*if (collision.gameObject.name == "StartEdge" && collision.gameObject.name == "EndEdge")
+        {
+            MovementSpeed = 0; 
+        }*/
     }
 
-    public void Damage(int amount) {
+    public void Damage(int amount)
+    {
         Life -= amount;
         if (Life <= 0)
             killMe();
@@ -71,15 +84,18 @@ public class Player : MonoBehaviour {
     public void Coin(int amount)
     {
         Coins += amount;
-        
+
     }
 
-    public void killMe() {
-        foreach (Collider2D collider in GetComponentsInChildren<Collider2D>()) {
+    public void killMe()
+    {
+        foreach (Collider2D collider in GetComponentsInChildren<Collider2D>())
+        {
             collider.enabled = false;
         }
         isAlive = false;
         anim.SetTrigger("Death");
         //Debug.Log("killMe");
     }
+
 }
