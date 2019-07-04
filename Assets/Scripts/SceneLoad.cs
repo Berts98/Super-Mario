384a1854c865cc0b60791153699011fff24d2a8c
Anna Bertolani <bertolani.anna@gmail.com>
2019-07-05 01:14:47 +0200

    finito main menu + aggiunta selezione livelli + aggiunto livello 2

---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoad : MonoBehaviour
+{
+	public void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/SceneLoad2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad2 : MonoBehaviour
+{
+    public void ChangeScene2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
