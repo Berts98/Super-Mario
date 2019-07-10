d8535b680faf755217301208d62ae0486a74f74c
Anna Bertolani <bertolani.anna@gmail.com>
2019-07-10 13:57:03 +0200

    prova movimento enemy

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour {
+
+    public List<Vector3> target = new List<Vector3>();
+    public float Speed;
+    private int targetIndex;
+    float targetValueX;
+	// Use this for initialization
+	void Start () {
+        targetIndex = 0;
+	}
+
+    private void Update()
+    {
+        if(target[targetIndex].x > transform.position.x)
+        {
+            targetValueX = Speed; 
+        }
+        else if(target[targetIndex].x < transform.position.x)
+        {
+            targetValueX = -Speed;
+        }
+        else
+        {
+            targetValueX = 0;
+        }
+        Move(new Vector3(targetValueX * Time.deltaTime, 0));
+        if(transform.position == new Vector3(target[targetIndex].x, transform.position.y, transform.position.z))
+        {
+            targetIndex++;
+            if(targetIndex >= target.Count)
+            {
+                targetIndex = 0;
+            }
+        }
+    }
+
+    private void Move(Vector3 _currentTarget)
+    {
+        transform.Translate(_currentTarget);
+        //Vector3.MoveTowards(transform.position, new Vector3(_currentTarget.x, transform.position.y, transform.position.z), Speed * Time.deltaTime);
+    }
+}
